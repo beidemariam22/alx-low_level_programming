9f05b87dc7792ab575f711dd199dc3e21be6dae7
beidemariam22 <94792047+beidemariam22@users.noreply.github.com>
2022-06-08 23:50:14 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,16 @@ int main(void)
 
 {
               
-	        	char *sh = "_putchar";
+     char *sh = "_putchar";
 
-			while (*sh)
+      while (*sh)
+            {
+	      _putchar(*sh);
 
-					{
+               sh++;
+             }
+             _putchar('\n');
 
-		          			_putchar(*sh);
-
-                                                    										sh++;
-                                    											}
-
-			         	_putchar('\n');
-
-					return (0);
+	     return (0);
 
 }
