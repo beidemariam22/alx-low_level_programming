fb27a17523653e677e736de1a9a796466eaf6d16
Beidemariam Awoke <beidemariam22@gmail.com>
2022-06-08 14:33:28 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+int main(void)
+
+{
+              
+	        	char *sh = "_putchar";
+
+			while (*sh)
+
+					{
+
+		          			_putchar(*sh);
+
+                                                    										sh++;
+                                    											}
+
+			         	_putchar('\n');
+
+					return (0);
+
+}
